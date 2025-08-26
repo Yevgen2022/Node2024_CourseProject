004c0cc752c1e4b20dd86bf460715b9395b2ad41
Yevhen Oshkukov <sy.yevgen.lc@gmail.com>
2025-08-26 15:23:50 +0200

    Create User class

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
   2. npm install --save-dev nodemon
 
   3. npm i sequelize , npm i mysql2, npm i randomstring
+
+  4. Create User class
+
+  
---
+++ db/index.js
@@ -0,0 +1,16 @@
+const Sequelize = require("sequelize");
+
+const sequelize =  new Sequelize("node_2024", "root", "", {
+    dialect: "mysql",
+    host: "127.0.0.1",
+    logging: false
+});
+
+const User = require('./User')(sequelize);
+const Authkey = require('./Authkey')(sequelize);
+
+module.exports = {
+    sequelize : sequelize,
+    user : User,
+    authkey : Authkey
+}
