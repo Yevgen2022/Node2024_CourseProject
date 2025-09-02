5db60cbeeed78de33e904a5afd73ef979528ce58
Yevhen Oshkukov <sy.yevgen.lc@gmail.com>
2025-09-02 19:46:58 +0200

    Nothing

--- src/utils/cookieOptions.js
+++ src/utils/cookieOptions.js
@@ -1,7 +1,7 @@
 // src/utils/cookieOptions.js
 module.exports = (overrides = {}) => ({
   httpOnly: true,
-  sameSite: 'lax',
+  sameSite: 'lax',     //safe default, works with forms/links
   secure: process.env.NODE_ENV === 'production',
   path: '/',                 // має збігатися при setCookie і clearCookie
   ...overrides,
