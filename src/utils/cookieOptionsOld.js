3a03fb673db599646550706c5c2ec79483bf97e7
Yevhen Oshkukov <sy.yevgen.lc@gmail.com>
2025-09-04 22:12:30 +0200

    Deleted not necessary files

--- src/utils/cookieOptionsOld.js
+++
@@ -1,8 +0,0 @@
-// src/utils/cookieOptions.js
-module.exports = (overrides = {}) => ({
-  httpOnly: true,
-  sameSite: 'lax',     //safe default, works with forms/links
-  secure: process.env.NODE_ENV === 'production',
-  path: '/',                 // має збігатися при setCookie і clearCookie
-  ...overrides,
-});
